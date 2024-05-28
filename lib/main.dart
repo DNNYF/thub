2568252ee994a9a1266552ba6ec7e43b4880e54c
DNNYF <albionku600@gmail.com>
2024-05-28 08:24:49 +0700

    merge collab folder

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomeScreen());
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      routes: {
+        "/" : (context) => HomePage(),
+        "cartPage" :(context) => CartPage(),
+      },
+    );
   }
-}
+}   
