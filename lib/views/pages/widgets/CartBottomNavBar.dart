b0a9bbc64c50ca34bb0ca98078b89f36e67040e1
Indra2901 <is29012003@gmail.com>
2024-06-04 09:09:07 +0700

    memperbaiki cart dan store

--- lib/views/pages/widgets/CartBottomNavBar.dart
+++ lib/views/pages/widgets/CartBottomNavBar.dart
@@ -1,56 +1,60 @@
-import 'package:vint/utils/exports.dart';
+import 'package:flutter/material.dart';
 
 class CartBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: Container(
-        color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        height: 500,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Total:",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Total:",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  "\Rp 90.000",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red,
-                  ),
+              ),
+              Text(
+                "\Rp 90.000",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
                 ),
-              ],
-            ),
-            Container(
-              alignment: Alignment.center,
-              height: 10,
+              ),
+            ],
+          ),
+          SizedBox(height: 10), // Beri jarak vertikal antara total dan tombol Checkout
+          InkWell(
+            onTap: () {
+              // Tambahkan fungsi onTap untuk tombol Checkout di sini
+            },
+            child: Container(
               width: double.infinity,
               decoration: BoxDecoration(
                 color: Colors.blue,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Text(
-                "Check Out",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+              padding: EdgeInsets.symmetric(vertical: 15), // Sesuaikan padding sesuai kebutuhan
+              child: Center(
+                child: Text(
+                  "Check Out",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/views/pages/widgets/PopularStoreWidget.dart
+++ lib/views/pages/widgets/PopularStoreWidget.dart
@@ -1,75 +1,78 @@
-import 'package:vint/utils/exports.dart';
+import 'package:flutter/material.dart';
 
 class PopularStoreWidget extends StatelessWidget {
   final List<String> popularStores = ["Store A", "Store B", "Store C", "Store D", "Store E"];
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: popularStores.map((store) {
-        return SizedBox(
-          width: MediaQuery.of(context).size.width * 0.16,
-          child: Card(
-            elevation: 3,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Stack(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.store,
-                        size: 50, // Ubah ukuran ikon
-                        color: Colors.black,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        store,
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.2, // Adjusted the height to be smaller
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: popularStores.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.45, // Adjusted width to fit 2 containers on the screen
+              child: Material(
+                elevation: 3, // Custom elevation for the circle icon
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 10, // Adjusted top position to center vertically within the container
+                        left: (MediaQuery.of(context).size.width * 0.45 - 60) / 2, // Adjusted to center the icon horizontally within the container
+                        child: Container(
+                          width: 60, // Width of the circle
+                          height: 60, // Height of the circle
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blue, // Changed the color to blue
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 3,
+                                blurRadius: 7,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Icon(
+                            Icons.store,
+                            size: 40, // Size of the icon
+                            color: Colors.white, // Changed the color to white
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Positioned(
-                  top: -15,
-                  left: MediaQuery.of(context).size.width * 0.08,
-                  child: Container(
-                    width: 80, // Ubah lebar logo
-                    height: 80, // Ubah tinggi logo
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 3,
-                          blurRadius: 7,
-                          offset: Offset(0, 3),
+                      Positioned(
+                        top: 100, // Positioned the text below the icon
+                        left: 0,
+                        right: 0,
+                        child: Center(
+                          child: Text(
+                            popularStores[index],
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                    child: Icon(
-                      Icons.store,
-                      size: 50, // Ubah ukuran ikon
-                      color: Colors.black,
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        );
-      }).toList(),
+          );
+        },
+      ),
     );
   }
 }
+
